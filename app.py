6f9853f5dc1c5da436dd970ff8370444045bd890
iryna-vokh-silence <iryna.vokh.des@gmail.com>
2024-10-18 21:46:57 +0300

    grammar corrections

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print("I'm learning Git merge conflicts" )
 name = input('Please enter your name:')
 last_name = input('Please enter your last name:')
-nick = input('Please enter your nickname')
+nick = input('Please enter your nickname:')
 print('Hi, {} {} {}!'.format(name, last_name, nick))
